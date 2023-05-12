1b0c557408a96de995b7ea08ab709656bfb092b5
killbasa <killbasab@gmail.com>
2023-05-12 16:13:16 -0400

    Redo install script

--- install.sh
+++ install.sh
@@ -1,33 +1,30 @@
 #!/usr/bin/env bash
 
-function linkdotfile {
-  file="$1"
-  ln -sfn ~/.dotfiles/$file ~/$file
+dotfiles_dir=~/dotfiles
+
+function rm_file {
+  sudo rm -rf $HOME/$1 > /dev/null 2>&1
 }
 
-[[ $(basename $(pwd)) == ".dotfiles" ]] ||
-  recho "Make sure you're in the ~/.dotfiles/ directory before running this"
+function link_dotfile {
+  ln -sf $dotfiles_dir/$1 $HOME/$1
+}
 
 if !(command -v brew > /dev/null); then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  sudo apt-get install build-essential
 fi
 
-brew install argocd
-brew install bat
-brew install fd
-brew install fish
-brew install fzf
-brew install hadolint
-brew install protobuf@3
-brew install bufbuild/buf/buf
-brew install derailed/k9s/k9s
+brew bundle --verbose --file="$HOME/dotfiles/Brewfile"
 
-linkdotfile .editorconfig
-linkdotfile .gitconfig
-linkdotfile .config
+rm_file .editorconfig
+rm_file .gitconfig
+rm_file .config
+
+link_dotfile .editorconfig
+link_dotfile .gitconfig
+link_dotfile .config
 
 echo "✓ Installation complete" >&2
 echo >&2
