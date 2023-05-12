df29eee98b16724c08d2bd6dd7c4a9757681f5fd
killbasa <killbasab@gmail.com>
2023-05-12 18:31:02 -0400

    Update

--- install.sh
+++ install.sh
@@ -2,33 +2,25 @@
 
 dotfiles_dir=~/dotfiles
 
-function rm_file {
+function link_file {
   sudo rm -rf $HOME/$1 > /dev/null 2>&1
-}
-
-function link_dotfile {
   ln -sf $dotfiles_dir/$1 $HOME/$1
 }
 
-if !(command -v brew > /dev/null); then
+if ! command -v brew > /dev/null; then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  /home/linuxbrew/.linuxbrew/bin/brew shellenv
 fi
 
 brew bundle --verbose --file="$HOME/dotfiles/Brewfile"
 
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 
-rm_file .editorconfig
-rm_file .gitconfig
-rm_file .config
+link_file .editorconfig
+link_file .gitconfig
+link_file .config
 
-link_dotfile .editorconfig
-link_dotfile .gitconfig
-link_dotfile .config
-
-echo >&2
 echo "✓ Installation complete" >&2
 echo >&2
 echo "Run the following to change the shell to Fish:" >&2
