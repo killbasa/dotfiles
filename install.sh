e3895e1c806272b2123fb336782bb2357ed9abc7
killbasa <killbasab@gmail.com>
2023-05-12 16:40:26 -0400

    Fix script eval

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ function link_dotfile {
 
 if !(command -v brew > /dev/null); then
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/home/sammy/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
