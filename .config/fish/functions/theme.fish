b9ed15491ceed0e83457d787226df469e310358a
killbasa <killbasab@gmail.com>
2024-01-11 22:44:07 -0500

    starship themes

---
+++ .config/fish/functions/theme.fish
@@ -0,0 +1,16 @@
+function theme
+	set THEMES "super" "text"
+
+	if not contains $argv $THEMES
+		echo "Invalid theme name, available themes: $THEMES"
+		return
+	end
+
+	set TARGET_THEME "$DOTFILES_HOME/.config/starship_themes/$argv.toml"
+
+	if test -e "$TARGET_THEME"
+		cat "$TARGET_THEME" | tee "$DOTFILES_HOME/.config/starship.toml" >/dev/null
+	else
+		echo "Theme $argv not found"
+	end
+end
