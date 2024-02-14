b8c5b9a7ba85594a96eaedcf68fd6dfc75e4db7c
killbasa <git@shaneb.xyz>
2024-02-14 16:58:42 -0500

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,12 @@ source $XDG_CONFIG_HOME/fish/conf.d/fisher.fish
 
 # Setup starship
 if type -q starship
+	function starship_transient_prompt_func
+		starship module character
+	end
+
 	starship init fish | source
+	enable_transience
 end
 
 if status is-interactive
